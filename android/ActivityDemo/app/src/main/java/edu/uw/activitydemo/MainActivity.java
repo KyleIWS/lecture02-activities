b6144244e288bf93bcecb141bddf55ccf07b9508
Kyle <kyleiws@uw.edu>
2017-03-30 15:08:32 -0700

    Work Kyle did today from March 30. Did not import something correctly I guess, need to figure why the symbol 'button' is not found. Get it boi, get it.

--- android/ActivityDemo/app/src/main/java/edu/uw/activitydemo/MainActivity.java
+++ android/ActivityDemo/app/src/main/java/edu/uw/activitydemo/MainActivity.java
@@ -2,12 +2,46 @@ package edu.uw.activitydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button butt = (Button) findViewById(R.id.button);
+        butt.setOnClickListener()
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.v(TAG, "You made it bruh, startin that thang");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.v(TAG, "made it to resume, my man");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.v(TAG, "You're a wizard Harry (stop)");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.v(TAG, "The end of The Iron Giant");
     }
 }
--- java/src/main/java/MyGUI.java
+++ java/src/main/java/MyGUI.java
@@ -8,22 +8,59 @@ import java.util.*;
 public class MyGUI extends JFrame {
 	
 	public MyGUI() {
-		super("The Skywalker Family");
+		super("Kyle GUI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton theButton = new JButton("Click me!");
+		JButton theButton = new JButton("Poke Kyle");
+		theButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// These are the actions yo
+				System.out.println("swiggity swooty");
+			}
+		});
 
+		JButton theButton2 = new JButton("Pinch Kyle :(");
+		theButton2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// These are the actions yo
+				System.out.println("im coming dat booty");
+			}
+		});
 
+		// Java div
+		JPanel container = new JPanel();
+		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+		container.add(theButton);
+		container.add(theButton2);
 
+		JPanel container2 = new JPanel();
+		container2.setLayout( new BoxLayout(container2, BoxLayout.X_AXIS));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("A"));
+		container2.add(new JButton("B"));
+		container2.add(new JButton("B"));
+		container.add(container2);
+		this.add(container);
 
-		
-		this.add(theButton);
-		
 		this.pack();
+		// This makes it visible.
 		this.setVisible(true);		
 	}
-	
+
 	public static void main(String[] args) {
-    new MyGUI(); //create the frame
-  }
+    	new MyGUI(); //create the frame
+  	}
 }
